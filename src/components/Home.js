c1b7c2aa3e233cc58ba73ed4632a643ba64dabab
yala0 <welcome.yanlafitte@gmail.com>
2023-02-02 12:18:24 +0100

    second commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import space from '../assets/space.jpg';
 const Home = () => {
    return (
       <div className='border-2 border-cyan-200'>
-         <img src={space} alt='space' className=' object-cover h-screen'/>
+         <img src={space} alt='space' className=' object-cover h-screen w-full'/>
          <Link
             to='/nasaphoto'
             className='absolute rounded px-4 py-2 hover:shadow-lg hover:shadow-cyan-200/50 hover:text-cyan-200  text-3xl tracking-wide top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  '
